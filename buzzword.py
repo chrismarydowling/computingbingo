5c355f01058f08681afcb5afe3913a89c62d4096
Nicholas Blott <blottn@tcd.ie>
2018-10-11 15:37:18 +0100

    added words

--- buzzword.py
+++ buzzword.py
@@ -2,19 +2,18 @@ import random
 
 words = ['bitcoin', 'overclock', 'moor\'s law',
          'edge', 'fog', 'cloud', 'power',
-         'synthesize']
+         'synthesize', 'compute', 'car', 'IOT'
+         'aggregate', 'distribute(d)', 'potential'
+         'cluster', 'protocol', 'network']
 
 players = 3
 
 out = []
 
 for i in range(0,players):
-    # select 25 random words
     out.append([])
     for _ in range(0, 25):
         num = random.randint(0,len(words) - 1)
         out[i].append(words[num])
-
-print(len(out[2]))
 
 
